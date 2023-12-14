c8adfe918cf443c58669679f7bc1f4b3935e2fb6
Zhanna <zh_23@list.ru>
2023-12-15 01:52:41 +0300

    3  tasks all finished

--- Home/Program.cs
+++ Home/Program.cs
@@ -1,5 +1,8 @@
-﻿//Задача 1
-void PrintArray(int[,] table)
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+void PrintArray(int[,] table) // метод печает массив
 {
   for (int i = 0; i < table.GetLength(0); i++)
   {
@@ -11,7 +14,7 @@ void PrintArray(int[,] table)
   }
 }
 
-void FillArray(int[,] tabl)
+void FillArray(int[,] tabl) //метод заполняет массив рандомными целыми числами от 1 до 9
 {
   for (int i = 0; i < tabl.GetLength(0); i++)
   {
@@ -22,21 +25,107 @@ void FillArray(int[,] tabl)
   }
 }
 
+void Find_Element (int [,] A) //метод ищет элемент с заданным индексом и выводит его значение
+{
+   Console.WriteLine("Задайте позицию элемента.");
+   Console.WriteLine("номер строки: ");
+   int str = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine("номер столбца: ");
+   int stlb = Convert.ToInt32(Console.ReadLine());
+
+   if (str >= A.GetLength(0) || stlb >= A.GetLength(1) || (str < 0) || (stlb < 0))
+    Console.WriteLine("Элемента с введенным индексом нет в таблице.");
+   else Console.WriteLine("Значение элемента с индексами [" + str + ", " + stlb + "]: " + A[str,stlb]);
+}
+
+void Replase_Lines (int[,] Arra) //метод принимает на вход массив и первую и последнюю строки местами
+{
+    int buf = 0;
+    for (int j = 0; j < Arra.GetLength(1); j++)
+    {
+        buf =Arra[0,j]; //копируем в буфер первую строку (индекс 0)
+        Arra[0,j] = Arra[Arra.GetLength(0)-1,j]; //в первую строку копируем последнюю
+        Arra[Arra.GetLength(0)-1,j] = buf;       //в последнюю пишем первую
+    }
+  Console.WriteLine();// Задача 2
+ PrintArray(Arra);    // Задача 2
+}
+
+
+void Find_Index (int [,] Mass) //метод ищет индекс строки с наименьшей суммой элементов
+{
+  int res_ind = 0;
+  int sum = 0;
+  int min = 0;
+  for (int i = 0; i < Mass.GetLength(0); i++) //идем по строкам
+  {
+       for (int j = 0; j < Mass.GetLength(1); j++) // идем по столбцам
+       {
+           sum += Mass[i,j]; 
+       }
+       //посчитали сумму строки  
+       Console.WriteLine("сумма " + i + "-ой строки: " + sum);
+       if (sum < min) 
+           {
+            min = sum;
+            res_ind = i;
+           }
+       sum = 0;
+  }
+    
+  Console.WriteLine("Индекс строки с наименьшей суммой элементов: " + res_ind);
+}
+
+
+void Find_Index1 (int [,] Mass) //метод ищет индекс ВСЕ индексы строк с наименьшей суммой элементов
+// если их в таблице получается несколько
+{
+  int [] mas_sum = new int [Mass.GetLength(0)];
+  //int res_ind = 0;
+  int sum = 0;
+  for (int i = 0; i < Mass.GetLength(0); i++) //идем по строкам
+  {
+       for (int j = 0; j < Mass.GetLength(1); j++) // идем по столбцам
+       {
+           sum += Mass[i,j]; 
+       }
+       //посчитали сумму строки  
+       Console.WriteLine("сумма " + i + "-ой строки: " + sum);
+       mas_sum[i] = sum;
+       sum = 0;
+  }
+   Console.WriteLine(String.Join(" ",mas_sum)); //получили массив сумм строк
+   string count = ""; //сюда будем записывать индексы минимальных значений
+   int min = mas_sum[0];
+
+   for (int h = 1; h < mas_sum.Length; h++)
+   {
+     if ((mas_sum[h] < min)) min = mas_sum[h]; 
+   }
+  Console.WriteLine("минимальное значение: " + min);
+  for (int h = 0; h < mas_sum.Length; h++)
+   {
+     if ((mas_sum[h] == min)) count = count + h + " "; 
+   }
+   Console.WriteLine("Индекс(ы) строк(и) с наименьшей суммой элементов: " + count);
+}
+
+
+//---------------Основное тело программы -----------------
 int[,] Sim  = new int [new Random().Next(2,6),new Random().Next(2,6)]; 
-//кол-во строк и столбцов задается рандомно от 2 до 5
-//PrintArray(Sim);
-//Console.WriteLine();
+//кол-во строк и столбцов задается рандомно от 2 до 5 
+
 FillArray(Sim);
 PrintArray(Sim);
+//Find_Element(Sim); // задача 1
 
-Console.WriteLine("Задайте позицию элемента.");
-Console.WriteLine("номер строки: ");
-int str = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("номер столбца: ");
-int stlb = Convert.ToInt32(Console.ReadLine());
+//Replase_Lines(Sim); // задача 2
 
-if (str >= Sim.GetLength(0) || stlb >= Sim.GetLength(1) || (str < 0) || (stlb < 0))
-    Console.WriteLine("Элемента с введенным индексом нет в таблице.");
-else Console.WriteLine("Значение элемента с индексами [" + str + ", " + stlb + "]: " + Sim[str,stlb]);
+
+//Find_Index(Sim); // задача 3
+//Find_Index1(Sim);  //Задача 3.1
+
+
+
 
 
